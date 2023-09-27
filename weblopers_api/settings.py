230a9faf27cb85f6718e733fdbf9f4e7bdd94427
OMID-ABDOLVAND <omidabdolvand.be@gmail.com>
2023-09-27 13:55:45 -0700

    add env files

--- weblopers_api/settings.py
+++ weblopers_api/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import environ
+env = environ.Env()
+environ.Env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -73,14 +73,16 @@ WSGI_APPLICATION = 'weblopers_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env['POSTGRES_DB'],
+        'USER': env['POSTGRES_USER'],
+        'HOST': env['POSTGRES_HOST'],
+        'PORT': env['POSTGRES_PORT'],
+        'PASSWORD': env['POSTGRES_PASSWORD'],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
