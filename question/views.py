0e23e9960a343e94029b38a5d70f36b7fdc61901
OMID-ABDOLVAND <omidabdolvand.be@gmail.com>
2023-09-27 18:22:46 -0700

    add initial view for questions

--- question/views.py
+++ question/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+
 
 # Create your views here.
+from question.models import Question
+from question.serializers import QuestionSerializer
+
+
+class QuestionViewSet(mixins.RetrieveModelMixin,
+                        mixins.CreateModelMixin,
+                        mixins.ListModelMixin,
+                        GenericViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
