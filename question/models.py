04154d3927ef4ddc6d53bbaa88e039bac072954b
OMID-ABDOLVAND <omidabdolvand.be@gmail.com>
2023-09-27 18:00:49 -0700

    add author to Question

--- question/models.py
+++ question/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from taggit.managers import TaggableManager
 from commen_models import TimeBasedModel
@@ -16,8 +17,9 @@ class Question(TimeBasedModel, models.Model):
         OPEN = 'open', 'Open'
         PENDING = 'pending', 'Pending Answer'
         FINISHED = 'finished', 'Finished'
-    title = models.CharField(max_length=255, )
-    description = models.TextField(max_length=1000, )
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='questions_author')
     image = models.ImageField()
     deadline = models.DateTimeField()
     tags = TaggableManager()
