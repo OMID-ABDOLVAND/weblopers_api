b3983aac9cfbaf6f4aa27ce85267ed23016ad982
OMID-ABDOLVAND <omidabdolvand.be@gmail.com>
2023-09-27 17:50:49 -0700

    add status-field & TimeBaseModel & __str__

--- question/models.py
+++ question/models.py
@@ -1,17 +1,28 @@
 from django.db import models
 from taggit.managers import TaggableManager
+from commen_models import TimeBasedModel
 
 
 # Create your models here.
 class Field(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
 
-class Question(models.Model):
+
+class Question(TimeBasedModel, models.Model):
+    class StatusChoices(models.TextChoices):
+        OPEN = 'open', 'Open'
+        PENDING = 'pending', 'Pending Answer'
+        FINISHED = 'finished', 'Finished'
     title = models.CharField(max_length=255, )
     description = models.TextField(max_length=1000, )
     image = models.ImageField()
     deadline = models.DateTimeField()
     tags = TaggableManager()
     fields = models.ManyToManyField(Field, related_name='questions_fields')
+    status = models.CharField(max_length=20, choices=StatusChoices.choices, default=StatusChoices.OPEN)
 
+    def __str__(self):
+        return self.title
