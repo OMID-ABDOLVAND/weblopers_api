02421f8e3d6aa789e55f0755764636f6a3ab9caf
OMID-ABDOLVAND <omidabdolvand.be@gmail.com>
2023-09-27 19:33:51 -0700

    add method fields

--- question/serializers.py
+++ question/serializers.py
@@ -7,6 +7,14 @@ from question.models import Question
 
 class QuestionSerializer(TaggitSerializer, serializers.ModelSerializer):
     tags = TagListSerializerField()
+    author = serializers.SerializerMethodField()
+    questions_fields = serializers.SerializerMethodField()
+
+    def get_questions_fields(self, obj):
+        return [field.name for field in obj.questions_fields.all()]
+
+    def get_author(self, obj):
+        return obj.author.username
 
     class Meta:
         model = Question
